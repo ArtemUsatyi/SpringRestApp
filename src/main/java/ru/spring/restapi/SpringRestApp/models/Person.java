7ca85816fe73547669571643557f062e6f06dea3
artemowork <artem180791@mail.ru>
2023-07-28 19:29:40 +0300

    Добавлена валидация - чтобы проверить правильность ввода данных об Человеке и если данные не верны, выбросить исключение

--- src/main/java/ru/spring/restapi/SpringRestApp/models/Person.java
+++ src/main/java/ru/spring/restapi/SpringRestApp/models/Person.java
@@ -1,6 +1,10 @@
 package ru.spring.restapi.SpringRestApp.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "People")
@@ -10,20 +14,28 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(name = "name")
+    @NotEmpty(message = "Имя не должно быть пустым")
+    @Size(min = 2, max = 40, message = "Имя должно состоять от 2 до 40 символов")
     private String name;
 
     @Column(name = "email")
+    @NotEmpty(message = "Email не должен быть пустым")
+    @Email(message = "Email не валидный")
     private String email;
     @Column(name = "age")
+    @Min(value = 0, message = "Год должен быть больше 0")
     private int age;
 
-    private Person(){}
+    public Person() {
+
+    }
 
     public Person(String name, String email, int age) {
         this.name = name;
         this.email = email;
         this.age = age;
     }
+
 
     public int getId() {
         return id;
