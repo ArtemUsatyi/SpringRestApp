61eab0efb82cc5a48e688c29042a694357931add
artemowork <artem180791@mail.ru>
2023-07-30 13:06:35 +0300

    В Controller исправлено отправка-принятия данных от клиента, а именно идет обработка данных клиента в DTO.

--- src/main/java/ru/spring/restapi/SpringRestApp/controllers/PeopleController.java
+++ src/main/java/ru/spring/restapi/SpringRestApp/controllers/PeopleController.java
@@ -1,12 +1,14 @@
 package ru.spring.restapi.SpringRestApp.controllers;
 
 import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import ru.spring.restapi.SpringRestApp.dto.PersonDTO;
 import ru.spring.restapi.SpringRestApp.models.Person;
 import ru.spring.restapi.SpringRestApp.services.PeopleService;
 import ru.spring.restapi.SpringRestApp.util.PersonNotCreateException;
@@ -14,29 +16,32 @@ import ru.spring.restapi.SpringRestApp.util.PersonNotFoundException;
 import ru.spring.restapi.SpringRestApp.util.PersonErrorResponse;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/people")
 public class PeopleController {
 
     private final PeopleService peopleService;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public PeopleController(PeopleService peopleService) {
+    public PeopleController(PeopleService peopleService, ModelMapper modelMapper) {
         this.peopleService = peopleService;
+        this.modelMapper = modelMapper;
     }
 
     @GetMapping
-    public List<Person> findAll() {
-        return peopleService.findAll();
+    public List<PersonDTO> findAll() {
+        return peopleService.findAll().stream().map(this::convertToPersonDTO).collect(Collectors.toList());
     }
 
     @GetMapping("{id}")
-    public Person findOne(@PathVariable("id") int id) {
-        return peopleService.findOne(id);
+    public PersonDTO findOne(@PathVariable("id") int id) {
+        return convertToPersonDTO(peopleService.findOne(id));
     }
     @PostMapping
-    public ResponseEntity<HttpStatus> create(@RequestBody @Valid Person person, BindingResult bindingResult){
+    public ResponseEntity<HttpStatus> create(@RequestBody @Valid PersonDTO personDTO, BindingResult bindingResult){
         if(bindingResult.hasErrors()) {
             StringBuilder errorsMes = new StringBuilder();
             List<FieldError> errors = bindingResult.getFieldErrors();
@@ -46,7 +51,7 @@ public class PeopleController {
             }
             throw new PersonNotCreateException(errorsMes.toString());
         }
-        peopleService.save(person);
+        peopleService.save(convertToPerson(personDTO));
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
@@ -59,6 +64,13 @@ public class PeopleController {
     @ExceptionHandler
     private ResponseEntity<PersonErrorResponse> handleException(PersonNotCreateException e) {
         PersonErrorResponse errorResponse = new PersonErrorResponse(e.getMessage(), System.currentTimeMillis());
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST); // BAD_REQUEST - ошибка, 404 статус
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST); // BAD_REQUEST - ошибка, 400 статус
+    }
+
+    public Person convertToPerson(PersonDTO personDTO){
+        return modelMapper.map(personDTO, Person.class);
+    }
+    public PersonDTO convertToPersonDTO(Person person){
+        return modelMapper.map(person, PersonDTO.class);
     }
 }
