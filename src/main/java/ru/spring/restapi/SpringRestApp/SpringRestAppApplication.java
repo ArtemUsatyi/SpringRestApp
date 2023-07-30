956cffdf29d9759b5635b4b3d25167b6e1268610
artemowork <artem180791@mail.ru>
2023-07-30 13:07:15 +0300

    добавлен новый Bean ModelMapper для внедрения в контроллер

--- src/main/java/ru/spring/restapi/SpringRestApp/SpringRestAppApplication.java
+++ src/main/java/ru/spring/restapi/SpringRestApp/SpringRestAppApplication.java
@@ -1,7 +1,9 @@
 package ru.spring.restapi.SpringRestApp;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringRestAppApplication {
@@ -10,4 +12,8 @@ public class SpringRestAppApplication {
 		SpringApplication.run(SpringRestAppApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
